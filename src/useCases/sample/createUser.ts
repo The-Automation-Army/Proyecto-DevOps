5ebb90fcddc690e8c7b8fb0576ddd22bee94364a
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-03-26 16:10:27 -0600

    :fire: remove old folder structure and sample code.

--- src/useCases/sample/createUser.ts
+++
@@ -1,8 +0,0 @@
-import { Prisma } from '@prisma/client'
-import prisma from '../../../libs/prisma'
-
-export const createUser = async (user: Prisma.UserCreateInput) => {
-  return await prisma.user.create({
-    data: user,
-  })
-}
