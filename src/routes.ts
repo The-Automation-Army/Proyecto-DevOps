1251583d12e0d996feb5b3cab66028619f483c6a
Jahir Gómez <jahirgomez1714@gmail.com>
2023-03-30 15:10:00 -0600

    ⚙ Adding routes.

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { userRoutes } from "./v1/routes/user.routes";
+import { animalRoutes } from "./v1/routes/animal.routes";
 
 const router = Router();
 router.use(userRoutes);
+router.use('/animal', animalRoutes)
 
 export { router };
---
+++ src/v1/routes/animal.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { createNewAnimal, findAnAnimalById, findManyAnimalsByName, findManyAnimalsByCategory, updateAnAnimalById, deleteAnAnimalById } from "../../controllers/animal.controller";
+import { ensureAuthenticated } from "../../middlewares/authentication";
+
+const animalRoutes = Router();
+
+animalRoutes.post("/newAnimal", ensureAuthenticated, createNewAnimal);
+animalRoutes.get("/findAnimalByID", findAnAnimalById);
+animalRoutes.get("/findAnimalsByName", findManyAnimalsByName);
+animalRoutes.get("/findAnimalsByCategory", findManyAnimalsByCategory);
+animalRoutes.put("/updateAnimalById", ensureAuthenticated, updateAnAnimalById);
+animalRoutes.delete("/deleteAnimalById", ensureAuthenticated, deleteAnAnimalById);
+
+
+export { animalRoutes };
