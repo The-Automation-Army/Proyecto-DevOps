ab3971a63108353307930f00a48cdb96070982a0
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-04-02 17:13:57 -0600

    :white_check_mark: add factories and complete user test cases

---
+++ tests/factories/UserFactory.ts
@@ -0,0 +1,44 @@
+import prisma from "../../libs/__mocks__/prisma";
+
+export const UserFactory = {
+  build: (attrs = {}) => {
+    const now = new Date("2023-03-25");
+    return {
+      id: 1,
+      username: "fake-username",
+      email: "fake@example.com",
+      password: "fake-password",
+      createdAt: now,
+      updatedAt: now,
+      ...attrs,
+    };
+  },
+  create: (attrs = {}) => {
+    const userMock = UserFactory.build(attrs);
+    prisma.user.create.mockResolvedValue(userMock);
+    return userMock;
+  },
+  find: (attrs = {}) => {
+    const userMock = UserFactory.build(attrs);
+    prisma.user.findFirst.mockResolvedValue(userMock);
+    return userMock;
+  },
+  findAll: () => {
+    const users = [
+      UserFactory.build({ id: 2, email: "rich@example.com" }),
+      UserFactory.build({ id: 3, email: "john@example.com" }),
+    ];
+    prisma.user.findMany.mockResolvedValue(users);
+    return users;
+  },
+  delete: (attrs = {}) => {
+    const user = UserFactory.build(attrs);
+    prisma.user.delete.mockResolvedValue(user);
+    return user;
+  },
+  update: (attrs = {}) => {
+    const user = UserFactory.build(attrs);
+    prisma.user.update.mockResolvedValue(user);
+    return user;
+  },
+};
--- tests/user.service.test.ts
+++ tests/user.service.test.ts
@@ -1,41 +1,65 @@
 import { describe, expect, it, vi } from "vitest";
 import prisma from "../libs/__mocks__/prisma";
-import { createUser, authUser } from "../src/services/user.service";
+import {
+  createUser,
+  authUser,
+  getAllUsers,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+} from "../src/services/user.service";
+import { UserFactory } from "./factories/UserFactory";
 
 vi.mock("../libs/prisma");
 
 describe("User service", () => {
   it("createUser should return the generated user", async () => {
-    const newUser = {
-      username: "Rich",
-      email: "rich@example.com",
-      password: "random-pw",
-      createdAt: new Date("2023-03-25"),
-      updatedAt: new Date("2023-04-26"),
-    };
-    
-    prisma.user.create.mockResolvedValue({ ...newUser, id: 1 });
+    const newUser = UserFactory.create();
     const user = await createUser(newUser);
 
     expect(user).toStrictEqual({ ...newUser, id: 1 });
   });
 
   it("authUser should return the generated token", async () => {
-    const foundUser = {
-      id: 1,
-      username: "rich",
-      email: "rich@example.com",
+    UserFactory.find({
       password: "$2a$08$80x7oi/B003dzs82KIs.BeThaj7C/MCUzeCClLQAFnxf1xVOZP8De",
-      createdAt: new Date("2023-03-25"),
-      updatedAt: new Date("2023-04-26"),
-    };
-
-    prisma.user.findFirst.mockResolvedValue(foundUser);
+    });
     const token = await authUser({
       email: "rich@example.com",
       password: "richPassword",
     });
 
     expect(token).toContain("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9");
+  });
+
+  it("getAllUsers should return all users", async () => {
+    const mockUsers = UserFactory.findAll();
+    const allUsers = await getAllUsers();
+
+    expect(allUsers).toStrictEqual([
+      { ...mockUsers[0], id: 2 },
+      { ...mockUsers[1], id: 3 },
+    ]);
+  });
+
+  it("deleteUserById should return a deleted user", async () => {
+    const mockUserToDelete = UserFactory.delete({ id: 11 });
+    const deletedUser = await deleteUserById(11);
+
+    expect(deletedUser).toStrictEqual(mockUserToDelete);
+  });
+
+  it("getUserById should return a user given an id", async () => {
+    const mockUser = UserFactory.find({ id: 6 });
+    const findUser = await getUserById(6);
+
+    expect(findUser).toStrictEqual(mockUser);
+  });
+
+  it("updateUserById should return a updated user given an id and data", async () => {
+    const mockUser = UserFactory.update({ username: "john" });
+    const updatedUser = await updateUserById(1, "john");
+
+    expect(updatedUser).toStrictEqual(mockUser);
   });
 });
