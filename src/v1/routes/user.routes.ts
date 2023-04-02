73aa41482165e0e118399d829ebb91b27af846b9
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-04-02 17:12:40 -0600

    :sparkles: add more user services

--- src/v1/routes/user.routes.ts
+++ src/v1/routes/user.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
-import { getAll, signIn, signUp } from "../../controllers/user.controller";
+import {
+  getAll,
+  getUser,
+  deleteUser,
+  signIn,
+  signUp,
+  updateUser,
+} from "../../controllers/user.controller";
 import { ensureAuthenticated } from "../../middlewares/authentication";
+import { Commons } from "./Commons";
 
 const userRoutes = Router();
 
-userRoutes.post("/register", signUp);
 userRoutes.post("/login", signIn);
+userRoutes.post("/register", signUp);
 userRoutes.get("/users", ensureAuthenticated, getAll);
+userRoutes.get("/users/:id", ensureAuthenticated, getUser);
+userRoutes.delete("/users/:id", ensureAuthenticated, deleteUser);
+userRoutes.put("/users/:id", ensureAuthenticated, updateUser);
+userRoutes.all("/users", Commons.methodNotAllowed);
 
 export { userRoutes };
