c5817b24bbfab1db62b55d97eb667c740fc39477
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-04-30 12:19:10 -0600

    :safety_vest: handle supplier exceptions

--- src/controllers/supplier.controller.ts
+++ src/controllers/supplier.controller.ts
@@ -1,36 +1,73 @@
 import { Request, Response } from "express";
-import { createSupplier, findSupplierById, findSuppliersByName, updateSupplierById, deleteSupplierById, findAllSuppliers} from "../services/supplier.service";
+import {
+  createSupplier,
+  deleteSupplierById,
+  findAllSuppliers,
+  findSupplierById,
+  findSuppliersByName,
+  updateSupplierById,
+} from "../services/supplier.service";
 
-export const createNewSupplier = async (req:Request, res:Response) => {
-    const {name, type, emaial, telephone, address} = req.body;
-    const supplier = await createSupplier (name, type, emaial, telephone, address);
+export const createNewSupplier = async (req: Request, res: Response) => {
+  const { name, type, email, telephone, address } = req.body;
+  try {
+    const supplier = await createSupplier({
+      name,
+      type,
+      email,
+      telephone,
+      address,
+    });
     return res.status(201).json(supplier);
+  } catch (error) {
+    return res.status(400).json({ message: "Missing or incorrect fields" });
+  }
 };
 
-export const findSuppliers = async () => {
-    const suppliers = await findAllSuppliers();
-    return suppliers;
-}
-
-export const findAnSupplierById = async (req:Request, res:Response) => {
-    const {id} = req.body;
-    const supplier = await findSupplierById(id);
-    return res.status(201).json(supplier);
+export const findSuppliers = async (_req: Request, res: Response) => {
+  const suppliers = await findAllSuppliers();
+  return res.status(200).json(suppliers);
 };
 
-export const findManySuppliersByName = async (req:Request, res:Response) => {
-    const {name} = req.body;
-    const supplier = await findSuppliersByName(name);
-    return res.status(201).json(supplier);
+export const findAnSupplierById = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const supplier = await findSupplierById(id);
+  if (!supplier) {
+    return res.status(404).json({ message: "Supplier not found" });
+  }
+  return res.status(200).json(supplier);
 };
 
-export const updateASupplierById = async (req:Request, res:Response) => {
-    const {id, name, type, emaial, telephone, address} = req.body;
-    const supplier = await updateSupplierById(id, name, type, emaial, telephone, address);
-    return res.status(201).json(supplier);
+export const findManySuppliersByName = async (req: Request, res: Response) => {
+  const params = req.query.name as string;
+  const suppliers = await findSuppliersByName(params);
+  return res.status(200).json(suppliers);
 };
 
-export const deleteASupplierById = async (req:Request, res:Response) => {
-    const {id} = req.body;
-    const supplier = await deleteSupplierById(id);
+export const updateASupplierById = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const { name, type, email, telephone, address } = req.body;
+  try {
+    const supplier = await updateSupplierById({
+      id,
+      name,
+      type,
+      email,
+      telephone,
+      address,
+    });
+    return res.status(200).json(supplier);
+  } catch (error) {
+    return res.status(404).json({ message: "Supplier not found" });
+  }
+};
+
+export const deleteASupplierById = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  try {
+    await deleteSupplierById(id);
+    return res.status(200).json({ message: "Supplier deleted" });
+  } catch (error) {
+    return res.status(404).json({ message: "Supplier not found" });
+  }
 };
