02aef9be9d0a756ee087f1d40808fef73bf2694f
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-05-02 20:29:38 -0600

    :white_check_mark: add tests for models

--- tests/supplier.service.test.ts
+++ tests/supplier.service.test.ts
@@ -1,64 +1,45 @@
 import { describe, expect, it, vi } from "vitest";
 import { SupplierFactory } from "./factories/SupplierFactory";
-import {
-  createSupplier,
-  deleteSupplierById,
-  findAllSuppliers,
-  findSupplierById,
-  findSuppliersByName,
-  updateSupplierById,
-} from "../src/services/supplier.service";
+import * as SupplierService from "../src/services/supplier.service";
 vi.mock("../libs/prisma");
 
 describe("Supplier service", () => {
-  it("createSupplier should return the stored supplier", async () => {
-    const mockSupplier = SupplierFactory.create();
-    const supplier = await createSupplier(mockSupplier);
-
-    expect(supplier).toStrictEqual({ ...mockSupplier, id: 1 });
-  });
-
-  it("findAllSuppliers should return all stored suppliers", async () => {
-    const mockSuppliers = SupplierFactory.finAll();
-    const suppliers = await findAllSuppliers();
+  it("findAll should return all stored suppliers", async () => {
+    const mockSuppliers = SupplierFactory.findAll();
+    const suppliers = await SupplierService.findAll();
 
     expect(suppliers).toStrictEqual(mockSuppliers);
     expect(suppliers.length).toBe(2);
   });
 
-  it("findSupplierById should return a supplier given a id", async () => {
-    const mockSupplier = SupplierFactory.findById({ id: 8 });
-    const findSupplier = await findSupplierById(8);
+  it("find should return a supplier given a id", async () => {
+    const mockSupplier = SupplierFactory.find({ id: 8 });
+    const findSupplier = await SupplierService.find(8);
 
     expect(findSupplier).toStrictEqual(mockSupplier);
   });
 
-  it("findSuppliersByName should return all matched suppliers given a term", async () => {
-    const mockSupplier = SupplierFactory.findByName({
-      name: "Fake-term-insensitive",
-    });
-    const suppliers = await findSuppliersByName("fake-term");
-    expect(suppliers).toStrictEqual(mockSupplier);
-    expect(suppliers.length).toBe(3);
+  it("create should return the stored supplier", async () => {
+    const mockSupplier = SupplierFactory.create();
+    const supplier = await SupplierService.create(mockSupplier);
+
+    expect(supplier).toStrictEqual({ ...mockSupplier, id: 1 });
   });
 
-  it("UpdateSupplierById should update the supplier's fields", async () => {
+  it("update should return updated the supplier's fields", async () => {
     const data = {
       name: "John",
       address: "Central Park 72nd Street",
     };
     const mockSupplier = SupplierFactory.update(data);
-    const updatedSupplier = await updateSupplierById({
-      id: 1,
-      ...data,
-    });
+    const updatedSupplier = await SupplierService.update(1, data);
 
     expect(updatedSupplier).toStrictEqual(mockSupplier);
   });
 
-  it("deleteSupplierById should update the supplier's fields", async () => {
+  it("remove should deleted supplier", async () => {
     const supplierToDelete = SupplierFactory.delete({ id: 3 });
-    const deletedUser = await deleteSupplierById(3);
+    const deletedUser = await SupplierService.remove(3);
 
     expect(deletedUser).toStrictEqual(supplierToDelete);
   });
