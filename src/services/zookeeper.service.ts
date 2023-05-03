c6f35f1d0a11522edd177771e8eb1a63ecd177c3
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2023-05-02 20:24:51 -0600

    :recycle: refactor api services
    rename service functions and add interfaces

--- src/services/zookeeper.service.ts
+++ src/services/zookeeper.service.ts
@@ -1,63 +1,66 @@
-import { Qualification, Role, ZookeepersCaringHabitats } from "@prisma/client";
 import prisma from "../../libs/prisma";
+import { IZookeeper } from "../dtos/zookeeper.dto";
 
 //Creation of a zookeeper.
-export async function createZookeeper(name:string, responsibility:Role, qualification:Qualification, salary:number, habitatsInfo:ZookeepersCaringHabitats[]) {
-    const zookeeper = await prisma.zookeeper.create({
-        data: {
-            name,
-            responsibility,
-            qualification,
-            salary,
-            habitats: {
-                create: habitatsInfo,
-            },
-        },
-    });
-    return zookeeper;
+export const create = async (newZookeeper: IZookeeper) => {
+  const zookeeper = await prisma.zookeeper.create({
+    data: {
+      name: newZookeeper.name,
+      responsibility: newZookeeper.responsibility,
+      qualification: newZookeeper.qualification,
+      salary: newZookeeper.salary,
+      habitats: {
+        create: newZookeeper.habitats,
+      },
+    },
+  });
+  return zookeeper;
 };
 
 //Read (query) of all zookeepers.
-export async function findAllZookeepers() {
-    const zookeepers = await prisma.zookeeper.findMany({});
-    return zookeepers;
-}
+export const findAll = async () => {
+  const zookeepers = await prisma.zookeeper.findMany({});
+  return zookeepers;
+};
 
 //Read (query) of a zookeeper by his ID.
-export async function findZookeeperById(requestedId:number) {
-    const zookeeper = await prisma.zookeeper.findUnique({
-        where: {
-            id: requestedId,
-        },
-    });
-    return zookeeper;
+export const find = async (requestedId: number) => {
+  const zookeeper = await prisma.zookeeper.findUnique({
+    where: {
+      id: requestedId,
+    },
+  });
+  return zookeeper;
 };
 
 //Update of a zookeeper by his ID.
-export async function updateZookeeperById(requestedId:number, modName:string, modResponsibility:Role, modQualification:Qualification, modSalary:number, modHabitatsInfo:ZookeepersCaringHabitats[]) {
-    const zookeeper = await prisma.zookeeper.update({
-        where: {
-            id: requestedId,
-        },
-        data: {
-            name: modName,
-            responsibility: modResponsibility,
-            qualification: modQualification,
-            salary: modSalary,
-            habitats: {
-                create: modHabitatsInfo,
-            },
-        },
-    });
-    return zookeeper
+export const update = async (
+  requestedId: number,
+  updateZookeeper: Partial<IZookeeper>
+) => {
+  const zookeeper = await prisma.zookeeper.update({
+    where: {
+      id: requestedId,
+    },
+    data: {
+      name: updateZookeeper.name,
+      responsibility: updateZookeeper.responsibility,
+      qualification: updateZookeeper.qualification,
+      salary: updateZookeeper.salary,
+      habitats: {
+        create: updateZookeeper.habitats,
+      },
+    },
+  });
+  return zookeeper;
 };
 
 //Delete of a zookeeper by his ID.
-export async function deleteZookeeperById(requestedId:number) {
-    const zookeeper = await prisma.zookeeper.delete({
-        where: {
-            id: requestedId,
-        },
-    });
-    return zookeeper;
+export const remove = async (requestedId: number) => {
+  const zookeeper = await prisma.zookeeper.delete({
+    where: {
+      id: requestedId,
+    },
+  });
+  return zookeeper;
 };
